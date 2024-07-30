bbff67ba9077f2c7c357a9cbb3023c51d644b99c
Qichen Liu <ql2326@nyu.edu>
2024-07-30 19:39:42 +0000

    fix Symbol for meta stock

--- app/realtimePrice.py
+++ app/realtimePrice.py
@@ -1,62 +1,3 @@
-# import requests
-# from datetime import datetime, timedelta
-#
-# ALPHA_VANTAGE_API_KEY = 'your_alpha_vantage_api_key'
-# ALPHA_VANTAGE_URL = 'https://www.alphavantage.co/query'
-#
-# def get_last_30_days_stock_prices(symbol):
-#     params = {
-#         'function': 'TIME_SERIES_DAILY',
-#         'symbol': symbol,
-#         'apikey': ALPHA_VANTAGE_API_KEY
-#     }
-#     response = requests.get(ALPHA_VANTAGE_URL, params=params)
-#     data = response.json()
-#     try:
-#         time_series = data['Time Series (Daily)']
-#         last_30_days_prices = []
-#         today = datetime.today()
-#         for i in range(30):
-#             date = (today - timedelta(days=i)).strftime('%Y-%m-%d')
-#             if date in time_series:
-#                 price = float(time_series[date]['1. open'])
-#                 last_30_days_prices.append((date, price))
-#         return last_30_days_prices
-#     except KeyError as e:
-#         print(f"KeyError: {e}")
-#         print(f"API Response: {data}")
-#         return None
-#     except Exception as e:
-#         print(f"Exception: {e}")
-#         return None
-#
-# print(get_last_30_days_prices('TSLA'))
-#
-#
-# def get_current_stock_price(symbol):
-#     params = {
-#         'function': 'TIME_SERIES_DAILY',
-#         'symbol': symbol,
-#         'apikey': ALPHA_VANTAGE_API_KEY
-#     }
-#     response = requests.get(ALPHA_VANTAGE_URL, params=params)
-#     data = response.json()
-#     try:
-#         time_series = data['Time Series (Daily)']
-#         most_recent_date = max(time_series.keys())
-#         price = float(time_series[most_recent_date]['1. open'])
-#         return price
-#     except KeyError as e:
-#         print(f"KeyError: {e}")
-#         print(f"API Response: {data}")
-#         return None
-#     except Exception as e:
-#         print(f"Exception: {e}")
-#         return None
-#
-# print(get_current_price('TSLA'))
-
-
 from datetime import datetime, timedelta
 import pandas as pd
 
@@ -82,3 +23,6 @@ def get_current_stock_price(symbol):
     price = float(df.loc[most_recent_date]['Adj Close'])
 
     return price
+
+
+print(get_last_30_days_stock_prices('META'))
